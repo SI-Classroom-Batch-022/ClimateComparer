0938927243343f81383dff0b43e5a062c93d30b2
Kim Reuter <kimbaerlyyo@web.de>
2025-05-26 21:31:46 +0200

    - Renamed properties in data classes for clarity - Created Composable for current location weather display - Started building MainScreen layout - Organized code into appropriate packages - Increased minSdk version to 26 - Added reusable dummy weather data for previews

---
+++ app/src/main/java/com/example/climatecomparer/ui/preview/DummyWeather.kt
@@ -0,0 +1,27 @@
+package com.example.climatecomparer.ui.preview
+
+import com.example.climatecomparer.data.model.City
+import com.example.climatecomparer.data.model.GeoLocation
+import com.example.climatecomparer.data.model.Weather
+import com.example.climatecomparer.data.model.WeatherState
+import java.time.LocalDateTime
+
+val dummyWeather = Weather(
+    city = City(
+        geoLocation = GeoLocation(
+            locationName = "Berlin",
+            latitude = 52.52,
+            longitude = 13.405,
+            countryCode = "DE",
+            state = "Berlin"
+        ),
+        isFavorite = true
+    ),
+    temperature = 21.5,
+    weatherState = WeatherState.SUNNY,
+    uvIndex = 5,
+    rainFall = 0.0,
+    windSpeed = 10.0,
+    windDirection = 135.0,
+    timeStamp = LocalDateTime.of(2023, 1, 1, 12, 0)
+)
